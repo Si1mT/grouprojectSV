590b6bbf479a1d801cb7035d03fa39ed398f7ff8
Si1mT <33309403+Si1mT@users.noreply.github.com>
2017-11-23 09:55:05 +0200

    idk

--- xd/xd/Program.cs
+++ xd/xd/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace xd
 {
@@ -10,6 +11,14 @@ namespace xd
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Ülesanne 2");
+            string text = System.IO.File.ReadAllText(@"C:\Users\opilane\Documents\GitHub\grouprojectSV\kttekst.txt");
+
+            string[] arr1 = new string[] { };
+            for (int i = 0; i < 1; i++)
+            {
+
+            }
         }
     }
 }
