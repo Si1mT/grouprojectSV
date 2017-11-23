dcdbde3edc2f20560fd0791efd6b6e377d32ccd1
Virk0 <virko.veelmaa@tptlive.ee>
2017-11-23 09:35:20 +0200

    cd

---
+++ xd/xd/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert the first number:");
+            int x = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Insert the second number");
+            int y = Int32.Parse(Console.ReadLine());
+            IEnumerable<int> range = Enumerable.Range(x, y);
+
+            foreach (var result in range)
+            {
+                if (result % 2 == 0)
+                {
+                    Console.WriteLine(result + " is Even Value");
+                }
+                else
+                {
+                    Console.WriteLine(result + " is Odd Value");
+                }
+            }
+        }
+    }
+}
