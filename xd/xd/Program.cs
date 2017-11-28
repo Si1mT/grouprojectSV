15b75a375980d6fd8aef640f3a0ab709dc6811cd
Virk0 <virko.veelmaa@tptlive.ee>
2017-11-28 08:25:40 +0200

    ..

--- xd/xd/Program.cs
+++ xd/xd/Program.cs
@@ -11,7 +11,7 @@ namespace xd
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
+
             Console.WriteLine("Insert the first number:");
             int x = Int32.Parse(Console.ReadLine());
             Console.WriteLine("Insert the second number");
@@ -36,14 +36,6 @@ namespace xd
             
             Console.WriteLine(text2);
 
-            int[] values1 = { 11, 15, 6, 13, 13, 25, 32, 11, 20, 5, 31, 16, 32, 29, 11, 13, 3, 29, 28, 24 };
-            int[] values2 = { 5, 19, 16, 4, 12, 7, 2, 28, 34, 29, 29, 36, 6, 8, 24, 18, 31, 7, 1, 7 };
-            int[] same = values1.Intersect(values2).ToArray();
-            int[] diff = values1.Union(values2).Except(same).ToArray();
-            int[] c = new int[] { diff[0], same[0], same[1], diff[1] };
-            Console.WriteLine(c);
-            Console.ReadLine();
-=======
             int[] values1 = {11, 15, 6, 13, 13, 25, 32, 11, 20, 5, 31, 16, 32, 29, 11, 13, 3, 29, 28, 24};
             int[] values2 = {5, 19, 16, 4, 12, 7, 2, 28, 34, 29, 29, 36, 6, 8, 24, 18, 31, 7, 1, 7};
             List<int> value1 = new List<int> { 11, 15, 6, 13, 13, 25, 32, 11, 20, 5, 31, 16, 32, 29, 11, 13, 3, 29, 28, 24 };
@@ -59,7 +51,6 @@ namespace xd
             {
                 Console.WriteLine(max1);
             }
->>>>>>> Harjutus-3
 
         }
     }
