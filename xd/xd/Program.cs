a44f6192d9aad05e4bbf4f13c3d9a5b9cb7e26a7
Virk0 <virko.veelmaa@tptlive.ee>
2017-11-28 08:28:54 +0200

    ......

--- xd/xd/Program.cs
+++ xd/xd/Program.cs
@@ -10,6 +10,20 @@ namespace xd
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Insert the first number:");
+            int x = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Insert the second number");
+            int y = Int32.Parse(Console.ReadLine());
+            IEnumerable<int> range = Enumerable.Range(x, y);
+
+            foreach (var result in range)
+            {
+                if (result % 2 == 0)
+                {
+                    Console.WriteLine(result + " is an even number");
+                }
+            }
+            Console.ReadLine();
             int[] values1 = {11, 15, 6, 13, 13, 25, 32, 11, 20, 5, 31, 16, 32, 29, 11, 13, 3, 29, 28, 24};
             int[] values2 = {5, 19, 16, 4, 12, 7, 2, 28, 34, 29, 29, 36, 6, 8, 24, 18, 31, 7, 1, 7};
             List<int> value1 = new List<int> { 11, 15, 6, 13, 13, 25, 32, 11, 20, 5, 31, 16, 32, 29, 11, 13, 3, 29, 28, 24 };
