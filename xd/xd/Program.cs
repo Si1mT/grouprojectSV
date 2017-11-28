e66236aa55bab041fe10fa65f3d6a7a89b5f453c
Virk0 <virko.veelmaa@tptlive.ee>
2017-11-28 08:23:38 +0200

    Kinda done

--- xd/xd/Program.cs
+++ xd/xd/Program.cs
@@ -14,7 +14,18 @@ namespace xd
             int[] values2 = {5, 19, 16, 4, 12, 7, 2, 28, 34, 29, 29, 36, 6, 8, 24, 18, 31, 7, 1, 7};
             List<int> value1 = new List<int> { 11, 15, 6, 13, 13, 25, 32, 11, 20, 5, 31, 16, 32, 29, 11, 13, 3, 29, 28, 24 };
             List<int> value2 = new List<int> { 5, 19, 16, 4, 12, 7, 2, 28, 34, 29, 29, 36, 6, 8, 24, 18, 31, 7, 1, 7 };
-            string[] value3 = value1.Except(value2).ToArray();
+            //string[] value3 = value1.Except(value2).ToArray();
+            var max1 = value1.Max();
+            var max2 = value2.Max();
+            if (max1 < max2)
+            {
+                Console.WriteLine(max2);
+            }
+            if (max2 < max1)
+            {
+                Console.WriteLine(max1);
+            }
+
         }
     }
 }
