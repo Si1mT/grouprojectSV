1de7aecdc0524c8db7cb43e33847e1890677809c
Si1mT <33309403+Si1mT@users.noreply.github.com>
2017-11-28 08:14:46 +0200

    võtsin merge HEAD jne ära

--- xd/xd/Program.cs
+++ xd/xd/Program.cs
@@ -11,8 +11,6 @@ namespace xd
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-<<<<<<< HEAD
             Console.WriteLine("Insert the first number:");
             int x = Int32.Parse(Console.ReadLine());
             Console.WriteLine("Insert the second number");
@@ -26,16 +24,7 @@ namespace xd
                     Console.WriteLine(result + " is an even number");
                 }
             }
-=======
-            int[] values1 = {11, 15, 6, 13, 13, 25, 32, 11, 20, 5, 31, 16, 32, 29, 11, 13, 3, 29, 28, 24};
-            int[] values2 = {5, 19, 16, 4, 12, 7, 2, 28, 34, 29, 29, 36, 6, 8, 24, 18, 31, 7, 1, 7};
-            int[] same = values1.Intersect(values2).ToArray(); 
-            int[] diff = values1.Union(values2).Except(same).ToArray();
-            int[] c = new int[] { diff[0], same[0], same[1], diff[1] };
-            Console.WriteLine(c);
->>>>>>> Harjutus-3
-            Console.ReadLine();
-=======
+         
             char[] chars = { ' ', ',', '.', ':', };
             string text = System.IO.File.ReadAllText("C:\\Users\\Siim\\Documents\\GitHub\\grouprojectSV\\kttekst.txt");
             //var result = text.Split("\n\r".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
@@ -46,10 +35,16 @@ namespace xd
             
 
             Console.WriteLine(text2);
+
+            int[] values1 = { 11, 15, 6, 13, 13, 25, 32, 11, 20, 5, 31, 16, 32, 29, 11, 13, 3, 29, 28, 24 };
+            int[] values2 = { 5, 19, 16, 4, 12, 7, 2, 28, 34, 29, 29, 36, 6, 8, 24, 18, 31, 7, 1, 7 };
+            int[] same = values1.Intersect(values2).ToArray();
+            int[] diff = values1.Union(values2).Except(same).ToArray();
+            int[] c = new int[] { diff[0], same[0], same[1], diff[1] };
+            Console.WriteLine(c);
             Console.ReadLine();
 
 
->>>>>>> Harjutus2
         }
     }
 }
